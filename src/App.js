0f18cc539cc2eb7d4fbe91a74ad5bc8f6eab2086
A-Little-Hat <soumyasarkar309@gmail.com>
2022-01-07 21:09:34 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import MyForm from './components/MyForm';
 
 function App() {
+  const [count, setCount] = useState([{
+    name: '',
+  }]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button type="button" onClick={() => {
+        setCount([...count, {
+          name: ''
+        }])
+      }}>
+        Add Form
+      </button>
+      <div className="App">
+        {
+          count.map(function (object, i) {
+            return <MyForm key={i} />;
+          })
+        }
+
+      </div>
+    </>
   );
 }
 
---
+++ src/components/MyForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MyForm() {
+    return (
+        <div>
+            <label htmlFor="name">name</label>
+            <input type="text" id="name" />
+        </div>
+    )
+}
+
+export default MyForm
